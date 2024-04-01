a23de4e3660d9611ad91e7153d168ca685c0e381
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-04-01 11:59:21 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect } from "react";
+import Header from "./components/Header";
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const url =
+  //       "https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10";
+  //     const options = {
+  //       method: "GET",
+  //       headers: {
+  //         "X-RapidAPI-Key":
+  //           "853339e32dmsh484669df916fa96p1fa47cjsn9dbd231055af",
+  //         "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  //       },
+  //     };
+
+  //     try {
+  //       const response = await fetch(url, options);
+  //       const result = await response.text();
+  //       const mario = result.split('{"kind"');
+  //       console.log(mario);
+  //       console.log(result);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Header/>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+const Header = () => {
+  return (
+    <nav className="flex justify-between text-xl">
+      <div className="flex items-center ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          viewBox="0 0 256 180"
+        >
+          <path
+            fill="#f00"
+            d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"
+          ></path>
+          <path
+            fill="#000"
+            d="m102.421 128.06l66.328-38.418l-66.328-38.418z"
+          ></path>
+        </svg>
+        <span>YouTube</span>
+      </div>
+      <div className="w-[20%] flex relative items-center">
+        <input
+          type="search"
+          className="w-full rounded-full pl-4 py-1"
+          placeholder="Search..."
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="black"
+          className="w-6 h-6 absolute right-0 top-[50%] translate-x-[-100%] translate-y-[-50%]"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
