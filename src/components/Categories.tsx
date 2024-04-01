7792f7b827bd1e7f56e8d6536e31f909e4144d56
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-04-01 16:49:01 +0200

    Finish aside

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -18,28 +18,32 @@ import {
 
 const Categories = () => {
   const icons = [
-    { icon: homeOutline, name: "Home" },
-    { icon: flameOutline, name: "Trending" },
-    { icon: codeSlashOutline, name: "Coding" },
-    { icon: logoJavascript, name: "JavaScript" },
-    { icon: logoReact, name: "ReactJS" },
-    { icon: musicalNotesOutline, name: "Music" },
-    { icon: schoolOutline, name: "Education" },
-    { icon: discOutline, name: "Podcast" },
-    { icon: filmOutline, name: "Movie" },
-    { icon: gameControllerOutline, name: "Gaming" },
-    { icon: pulseOutline, name: "Live" },
-    { icon: footballOutline, name: "Sport" },
-    { icon: sparklesOutline, name: "Fashion" },
-    { icon: diamondOutline, name: "Beauty" },
+    { isActive: true, icon: homeOutline, name: "Home" },
+    { isActive: false, icon: flameOutline, name: "Trending" },
+    { isActive: false, icon: codeSlashOutline, name: "Coding" },
+    { isActive: false, icon: logoJavascript, name: "JavaScript" },
+    { isActive: false, icon: logoReact, name: "ReactJS" },
+    { isActive: false, icon: musicalNotesOutline, name: "Music" },
+    { isActive: false, icon: schoolOutline, name: "Education" },
+    { isActive: false, icon: discOutline, name: "Podcast" },
+    { isActive: false, icon: filmOutline, name: "Movie" },
+    { isActive: false, icon: gameControllerOutline, name: "Gaming" },
+    { isActive: false, icon: pulseOutline, name: "Live" },
+    { isActive: false, icon: footballOutline, name: "Sport" },
+    { isActive: false, icon: sparklesOutline, name: "Fashion" },
+    { isActive: false, icon: diamondOutline, name: "Beauty" },
   ];
 
+  const isActive = "bg-[#ff0000] custom-white rounded-full ";
+  const baseClass =
+    "flex items-center gap-3 custom-color hover:bg-[#ff0000] hover:text-white hover:rounded-full px-2";
+
   return (
-    <aside className="pt-12 px-2 flex flex-col justify-center gap-4 border w-[10%]">
+    <aside className="pt-12 px-2 flex flex-col justify-center gap-6 border w-[10%]">
       {icons.map((icon, index) => (
         <button
           key={index}
-          className="flex items-center gap-4 custom hover:bg-[#ff0000] hover:text-white hover:rounded-full px-2"
+          className={`${baseClass} ${icon.isActive ? isActive : ""}`}
         >
           <IonIcon icon={icon.icon} size="large"></IonIcon>
           <p className="text-white">{icon.name}</p>
