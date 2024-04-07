dbca3fa4a0f03d10002ecc3e37de320fc7fef000
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-04-07 15:40:34 +0200

    Add time stamps when video was published

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,5 +1,6 @@
 const VideoCard = ({ el }) => {
-  console.log(el);
+  // console.log(el);
+  const currentDate = new Date();
   const {
     id: { kind: kindID, channelId },
     kind,
@@ -18,16 +19,39 @@ const VideoCard = ({ el }) => {
     kindID === "youtube#channel"
       ? "w-40 h-40 rounded-full flex items-center justify-center"
       : "w-full h-full";
+  const publishedDate = new Date(publishTime.substring(0, 10));
+
+  function formatTimeDifference(curDate, pubDate) {
+    const timeDifference = curDate.getTime() - pubDate.getTime();
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (daysDifference === 0) {
+      return "today";
+    } else if (daysDifference === 1) {
+      return "1 day ago";
+    } else if (daysDifference < 7) {
+      return `${daysDifference} days ago`;
+    } else if (daysDifference <= 30) {
+      const weeksDifference = Math.floor(daysDifference / 7);
+      return `${weeksDifference} week${weeksDifference > 1 ? "s" : ""} ago`;
+    } else if (daysDifference <= 365) {
+      const monthsDifference = Math.floor(daysDifference / 30);
+      return `${monthsDifference} month${monthsDifference > 1 ? "s" : ""} ago`;
+    } else {
+      const yearsDifference = Math.floor(daysDifference / 365);
+      return `${yearsDifference} year${yearsDifference > 1 ? "s" : ""} ago`;
+    }
+  }
 
   return (
-    <div className="flex flex-col justify-between h-[300px]">
+    <div className="flex flex-col justify-between ">
       <div className="h-[200px] flex justify-center items-center cursor-pointer">
         <img src={high.url} alt="Some img alt" className={styling} />
       </div>
-      <div className="bg-stone-900 h-[100px] flex flex-col gap-1 p-2">
+      <div className="bg-stone-900 h-[120px] flex flex-col gap-1 p-2">
         <h2>{title}</h2>
         <h3 className="text-stone-400">{channelTitle}</h3>
-        <h4>2 years ago</h4>
+        <h4>{formatTimeDifference(currentDate, publishedDate)}</h4>
       </div>
     </div>
   );
