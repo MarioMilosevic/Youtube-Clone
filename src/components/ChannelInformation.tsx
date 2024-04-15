33f81618c4aa8973b90d01526a377ebba9bea17b
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-04-15 14:45:08 +0200

    Finish channel information component

--- src/components/ChannelInformation.tsx
+++ src/components/ChannelInformation.tsx
@@ -1,28 +1,36 @@
-import slika from "../assets/probno.jpg"
 type ChannelInformationTypes = {
   channelDetails: {
-    description: string;
-    title: string;
-}
-}
+    brandingSettings: {
+      image: {
+        bannerExternalUrl: string;
+      };
+    };
+    snippet: {
+      description: string;
+      title: string;
+    };
+  };
+};
 
-const ChannelInformation = ({ channelDetails }) => {
-  console.log("ovo je proslijedjeno",channelDetails)
-  // const {
-  //   description,
-  //   title,
-   
-  // } = channelDetails;
+const ChannelInformation = ({ channelDetails }:ChannelInformationTypes) => {
+  const {
+    brandingSettings: {
+      image: { bannerExternalUrl },
+    },
+    snippet: { description, title },
+  } = channelDetails;
 
   return (
     <header className="flex flex-col items-center w-[1300px] mx-auto">
       <div className="w-56 h-56 rounded-full">
-        <img className="rounded-full" src={slika} alt="Neki text" />
+        <img
+          className="w-full h-full rounded-full object-cover"
+          src={bannerExternalUrl}
+          alt={title}
+        />
       </div>
-      <h2 className="pt-4 text-3xl">Neki naslov</h2>
-      <div className="py-4">
-      Neki opis
-      </div>
+      <h2 className="pt-4 text-3xl">{title}</h2>
+      <div className="py-4">{description}</div>
     </header>
   );
 };
